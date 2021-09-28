ede03c4b4647dd84d231e2c3d279daebda43ccef
DerianCordobaPerez <derianunanleon@gmail.com>
2021-09-28 15:49:55 -0600

    Modificacion de las rutas y del modelo de Socio

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\EmployeeAuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PartnerController;
+use App\Http\Controllers\RequestController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserRoleController;
 use Illuminate\Support\Facades\Route;
@@ -16,12 +19,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/login', [EmployeeAuthController::class, 'render_login'])->name('auth.employee.login');
 
-Route::group([], function() {
+Route::group([
+    'prefix' => 'dev',
+    'middleware' => ['auth']
+], function() {
+
+    Route::get('/', [HomeController::class, 'home'])->name('home');
 
     Route::resource('userRoles', UserRoleController::class);
 
     Route::resource('roles', RoleController::class);
+
+    Route::resource('partners', PartnerController::class);
+
+    Route::resource('requests', RequestController::class);
 
 });
